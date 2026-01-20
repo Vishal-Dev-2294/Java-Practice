7c4449646f89dabddecf064dd7a8eb99d2e390a6
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-20 19:22:32 +0530

    Understanding boiler plate done

---
+++ 2 Java Basics/FIrstProgram/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println("Wellcome");
+        System.out.println("Wellcome Vishal");
+    }
+}
--- 2 Java Basics/FirstProject/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-     System.out.println("Vishal is very dangeour");
-        System.out.println("Vishal is very dangeourrrrr");
-    }
-}
