78eb8a41396219f86f820f090eb28bc482b2aae4
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-22 00:00:12 +0530

    Practice Never Stop

--- Practice Topics/src/UserInput.java
+++ Practice Topics/src/UserInput.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
-public class UserInput {
+class Welcome {
+    public static void main(String[] args) {
+        System.out.print("Please enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Welcome " + name + " to KG Coding");
+    }
 }
