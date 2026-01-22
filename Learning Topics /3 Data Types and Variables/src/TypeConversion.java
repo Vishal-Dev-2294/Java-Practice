8ad4565256d85f8aef0e21fb1bc1d68352974a1b
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-22 11:14:41 +0530

    understanding operators...

---
+++ Learning Topics /3 Data Types and Variables/src/TypeConversion.java
@@ -0,0 +1,9 @@
+public class TypeConversion {
+    public static void main(String[] args) {
+        float myNum = 5;
+        System.out.println(myNum);
+
+        int myInt = (int) 5.45f;
+        System.out.println(myInt);
+    }
+}
---
+++ Practice Topics/src/SumofTwoNumbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class SumofTwoNumbers {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.print("Enter your first number: ");
+        int firstNum = Input.nextInt();
+        System.out.print("Enter your second number: ");
+        int secondNum = Input.nextInt();
+        int sum = firstNum + secondNum;
+        System.out.println("your sum is: " + sum);
+    }
+}
