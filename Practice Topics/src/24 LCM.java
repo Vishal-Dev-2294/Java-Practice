98acf83890c467beb36fa4c80ae5058ad5684b95
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-27 20:49:23 +0530

    done

--- Practice Topics/src/24 LCM.java
+++ Practice Topics/src/24 LCM.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LCM {
+class LCM {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter Your Number : ");
