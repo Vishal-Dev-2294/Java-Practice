42b3254b6315c8d84c0bce2e06a0e7d483aac6d6
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-29 15:37:39 +0530

    donr

--- Practice Topics/src/30 PalindromeNumber.java
+++ Practice Topics/src/30 PalindromeNumber.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeNumber {
+class PalindromeNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Palindrome Number");
