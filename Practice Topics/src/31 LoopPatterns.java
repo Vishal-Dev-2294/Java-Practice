fedeac89c795479cd2938201dcefe1a304af42ac
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-01-30 12:59:03 +0530

    done

--- Practice Topics/src/31 LoopPatterns.java
+++ Practice Topics/src/31 LoopPatterns.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LoopPatterns {
+class LoopPatterns {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("We are here to print patterns\n");
--- Practice Topics/src/ArraySumAverage.java
+++ Practice Topics/src/ArraySumAverage.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class ArraySumAverage {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Wellcome to sum and average ");
+        System.out.print("Enter the size of the element : ");
+        int arraySize = input.nextInt();
+        int[] nums = new int[arraySize];
+        int i = 0;
+        while(i < arraySize){
+            System.out.print("Please enter element no " + (i+1) + ": ");
+            nums[i] = input.nextInt();
+            i++;
+        }
     }
 }
