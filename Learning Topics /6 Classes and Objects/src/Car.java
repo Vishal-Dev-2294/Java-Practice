5ba6e36de244ac11b3c672bb9cc7b7b1cb6d7187
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-02-01 02:51:54 +0530

    done

--- Learning Topics /6 Classes and Objects/src/Car.java
+++ Learning Topics /6 Classes and Objects/src/Car.java
@@ -5,15 +5,32 @@ public class Car {
     float currentFuelInLiters;
     int noOfSeats;
 
-    public void drive() {
-        System.out.println("Car is Driving");
+    public Car start() {
+        if(currentFuelInLiters == 0){
+            System.out.println("Car is out of fuel");
+        } else if (currentFuelInLiters < 5){
+            System.out.println("Car is in reserved mode , please refuel");
+            currentFuelInLiters--;
+        } else {
+            System.out.println("Car is started");
+        }
+        return this;
     }
 
-    public void addFuel() {
-        currentFuelInLiters++;
+    public void drive() {
+        System.out.println("Car is Driving");
+        currentFuelInLiters--;
+    }
+
+    public void addFuel(float currentFuelInLiters) {
+        this.currentFuelInLiters += currentFuelInLiters;
     }
 
     public float getCurrentFuelLevel() {
         return currentFuelInLiters;
+    }
+
+    public void myColor(String color) {
+        System.out.println("Your car color is " + color);
     }
 }
--- Learning Topics /6 Classes and Objects/src/Driver.java
+++ Learning Topics /6 Classes and Objects/src/Driver.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
-
 public class Driver {
+    static int minAgeForDriving = 18;
+
+    String name;
+    int age;
+    String dateOfLicense;
+
+    public boolean isAllowedToDrive() {
+        return this.age >= minAgeForDriving;
+    }
+
+    public static void main(String[] args) {
+//        Car mycar = new Car();
+//        mycar.myColor("red");
+//        mycar.addFuel(7);
+//        mycar.drive();
+//        mycar.drive();
+//        mycar.drive();
+//        mycar.addFuel(6);
+//        mycar.drive();
+//        System.out.println(mycar.getCurrentFuelLevel());
+
+        Car bmw = new Car();
+        bmw.addFuel(6);
+        bmw.start().drive();
+        bmw.addFuel(10);
+        bmw.getCurrentFuelLevel();
+    }
 }
