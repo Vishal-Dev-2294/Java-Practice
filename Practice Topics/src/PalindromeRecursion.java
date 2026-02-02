32a2759972caa2904a7eae37ed11ba8ea95e7b79
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-02-03 00:50:49 +0530

    done

---
+++ Practice Topics/src/PalindromeRecursion.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeRecursion {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the string to be checked : ");
+        String str = input.next();
+        System.out.println("Your String is " +
+                ((isPalindrome(str) ? "palindrome"
+                                    : "not palindrome" )));
+    }
+
+    public static boolean isPalindrome(String str){
+        if (str.length() <= 1){
+            return true;
+        }
+        int lastPosition = str.length() - 1;
+        if (str.charAt(0) != str.charAt(lastPosition)){
+            return false;
+        }
+        String newStr = str.substring(1 , lastPosition);
+        return isPalindrome(newStr);
+    }
+}
