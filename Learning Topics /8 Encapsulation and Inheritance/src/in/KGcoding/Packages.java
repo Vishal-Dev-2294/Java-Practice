88aaac9b237b06b5f2d6f7442d208a125ba47b62
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-02-04 17:52:14 +0530

    done

---
+++ Learning Topics /8 Encapsulation and Inheritance/src/in/KGcoding/Packages.java
@@ -0,0 +1,10 @@
+package in.KGcoding;
+
+import java.util.Scanner;
+
+public class Packages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        in.knowledgeGate.Packages packages = new in.knowledgeGate.Packages();
+    }
+}
