832f64007153e88c0aacfc7a6aa01c8aacb46737
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-02-22 23:33:58 +0530

    done

--- Learning Topics /10 Exception and File Handling/src/in/KgCoding/exception/Calculator.java
+++ Learning Topics /10 Exception and File Handling/src/in/KgCoding/exception/Calculator.java
@@ -4,6 +4,19 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+        a();
+    }
+
+    private static void a(){
+        b();
+    }
+    private static void b(){
+        c();
+    }
+    private static void c(){
+        d();
+    }
+    private static void d(){
         Scanner input =  new Scanner(System.in);
         System.out.print("Enter the first number : ");
         int first = input.nextInt();
@@ -16,10 +29,12 @@ public class Calculator {
             System.out.printf("result is %d" , a[6]);
         } catch (ArithmeticException e) {
             System.out.printf("%s , enter valid values " , e.getMessage());
-        }  catch (ArrayIndexOutOfBoundsException e) {
-            System.out.printf(e.getMessage());
-        } catch (Throwable th){
+        }  catch (Throwable th){
             System.out.println("bhai kuch to galat kiya hai ");
+            throw th;
+        } finally {
+            System.out.println(" i am in finally block ");
         }
     }
+
 }
