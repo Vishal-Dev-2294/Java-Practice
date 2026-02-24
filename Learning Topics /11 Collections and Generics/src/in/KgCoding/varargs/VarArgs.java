7f49f7f3d95d6187cc141eb216f78a827b7a06f0
Vishal Kushwaha <vishalkushwaha2294@gmail.com>
2026-02-24 19:52:38 +0530

    done

--- Learning Topics /11 Collections and Generics/src/in/KgCoding/varargs/VarArgs.java
+++ Learning Topics /11 Collections and Generics/src/in/KgCoding/varargs/VarArgs.java
@@ -1,5 +1,20 @@
 package in.KgCoding.varargs;
 
 public class VarArgs {
+    public static void main(String[] args) {
+        System.out.println(sum(2,5));
+        System.out.println(Sum(new int[] {2,3,4,5,6}));
+    }
 
+    public static int Sum(int[] a){
+        int sum = 0;
+        for (int i : a) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int sum(int a , int b){
+        return a + b;
+    }
 }
